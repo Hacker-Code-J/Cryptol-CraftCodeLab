9ebb0968fd9f0d005b86097cd512bfc02b7e8f53
Hacker-Code-J <hacker3740@gmail.com>
2024-07-23 12:41:50 +0900

    Laptop240723v1 - Renewal

--- void_verification/arithmetic.c
+++
@@ -1,7 +0,0 @@
-#include <stdint.h>
-
-void void_add(uint16_t a, uint16_t b, uint32_t *result) {
-    *result = (uint32_t)a + (uint32_t)b;
-}
-
-// clang -O2 -emit-llvm -c arithmetic.c -o arithmetic.bc
